d3ac3f83e00fb2e87c6ad97f29697ff5fc8fbf74
Partha Das <parthodasm@gmail.com>
2025-12-25 20:47:49 +0600

    Add jwt and bcrypt for providing better user experiance and authentication

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:5000/api/register", {
+        name,
+        email,
+        password,
+      });
+
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="m-20">
+      <form className="flex flex-col" onSubmit={onSubmit}>
+        <h1>Time Travel Register</h1>
+        <div className="flex flex-col gap-10">
+          <input
+            type="text"
+            value={name}
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            value={password}
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ENV } from "./lib/ENV.js";
 import travelRoute from "./routes/travel.js";
+import userRoute from "./routes/user.js";
 import connectDB from "./lib/connectDB.js";
 import cors from "cors";
 const app = express();
@@ -12,6 +13,7 @@ connectDB();
 app.use(cors());
 
 app.use("/api", travelRoute);
+app.use("/api", userRoute);
 
 app.listen(ENV.PORT || 5000, () => {
   console.log("Server running on the port", ENV.PORT);
