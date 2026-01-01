3e19d9757d289ec2d899489609a549c9e6d090a0
Partha Das <parthodasm@gmail.com>
2026-01-01 09:28:00 +0600

    Fix the problem where user data wastn't showing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios"
 import TravelForm from "../components/TravelForm";
 import ResultCard from "../components/ResultCard";
-
+import {useNavigate} from "react-router-dom"
 const Home = () => {
+  const [data, setData] = useState(null);
+  const navigate=useNavigate()
   const getToken = async () => {
     try {
       const token = localStorage.getItem("accessToken");
 
-      await axios.get("http://localhost:5000/api/home", {
+      const res = await axios.get("http://localhost:5000/api/home", {
         headers: { Authorization: `Bearer ${token}` },
         withCredentials: true,
       });
+      console.log(res.data);
+      setData(res.data);
     } catch (error) {
       if (error.response?.status === 401) {
         try {
@@ -19,7 +24,10 @@ const Home = () => {
             {},
             { withCredentials: true }
           );
-          localStorage.setItem("accessToken", refreshRes.data.accessToken);
+          const newToken = refreshRes.data.accessToken;
+          localStorage.setItem("accessToken", newToken);
+
+          return getToken();
         } catch (error) {
           localStorage.removeItem("accessToken");
           navigate("/login");
@@ -31,11 +39,14 @@ const Home = () => {
     getToken();
   }, []);
 
+  console.log(data);
+
   return (
     <div className="min-h-screen p-6 bg-slate-100">
       <h1 className="text-2xl text-center text-blue-900 font-semibold">
         TxTravel (Time Travel Simulator)
       </h1>
+
       <p className="text-sm text-center text-gray-500 mt-1">
         This isn’t magic. It’s time travel powered by science.
       </p>
--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -4,10 +4,10 @@ import { ENV } from "../lib/ENV.js";
 const verifyToken = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-    if (!authHeader) return res.status(401);
+    if (!authHeader) return res.sendStatus(401);
 
     const token = authHeader.split(" ")[1];
-    if (!token) return res.status(401);
+    if (!token) return res.sendStatus(401);
 
     jwt.verify(token, ENV.ACCESS_SECRET, (err, decoded) => {
       if (err) return res.sendStatus(401);
